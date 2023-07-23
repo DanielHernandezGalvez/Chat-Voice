96bf8a8748e2c427012236026d1c300c2fa5ac38
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-23 10:16:34 -0600

    Update and rename App.js to App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,44 @@
+import React, { useState } from "react";
+import SpeechRecognition from "./components/SpeechRecognition";
+import VoiceRecognition from "./components/VoiceRecognition";
+
+const App: React.FC = () => {
+  const [change, setChange] = useState<boolean>(false);
+
+  const handleText = (): void => {
+    setChange(false);
+  };
+
+  const handleVoice = (): void => {
+    setChange(true);
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-primary bg-primary px-4 ">
+        <button
+          className="btn btn-primary border-none p-0"
+          onClick={handleText}
+        >
+          Voz a texto
+        </button>
+        <button
+          className="btn btn-primary border-none p-0"
+          onClick={handleVoice}
+        >
+          Texto a Voz
+        </button>
+      </nav>
+
+      {change ? <SpeechRecognition /> : <VoiceRecognition />}
+    </>
+  );
+};
+
+export default App;
+
+
+/*
 // import { useUser } from "./context/UserContext";
 import SpeechRecognition from "./components/SpeechRecognition";
 import React, { useState } from "react";
